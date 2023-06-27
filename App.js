756c20a5ecb40d3fd657b36ee6f9f34777897b2e
JimmyCodemunity <jameswafula2002@gmail.com>
2023-06-27 21:22:37 +0300

    firebase RN

--- App.js
+++ App.js
@@ -1,12 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity,Alert} from 'react-native';
+import React, {useState} from 'react'
+
+import {getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword} from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebase-config';
+
+//navigation stack
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator;
+
+
+
+
+function HomeScreen(){
+  return(
+    <View style={styles.home}>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+
+function LoginScreen(){
+  const [email,setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const navigation = useNavigation;
+
+  const handleCreateAccount = () =>{
+    createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=>{
+      Alert.alert('Account created successfully');
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error)=>{
+      console.log(error);
+      Alert.alert(error.message);
+    })
+  }
+
+
+  const handleSignIn = () =>{
+    signInWithEmailAndPassword(auth,email,password)
+    .then((userCredential)=>{
+      console.log('User signed in!');
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) =>{
+      console.log(error)
+    })
+  }
+
+
+
+  return(
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{fontSize:34,fontWeight:'bold'}}>Login</Text>
+      </View>
+
+      <View style={styles.loginContainer}>
+        <TextInput style={styles.input}
+        onChangeText={(text)=> setEmail(text)}
+         placeholder='Enter email'/>
+
+
+        <TextInput style={styles.input}
+         secureTextEntry
+         onChangeText={(text)=> setPassword(text)}
+          placeholder='Enter password'/>
+      </View>
+      <TouchableOpacity onPress={handleSignIn} style={styles.logbtn}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleCreateAccount} style={styles.regbtn}>
+        <Text>Create Account</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer> 
   );
 }
 
@@ -17,4 +108,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer:{
+    width:'100%',
+    
+
+  },
+  input:{
+    
+    borderWidth:1,
+    borderColor:'#ccc',
+    height:30,
+    width:280,
+   
+    borderRadius:5,
+    marginBottom:15,
+  },
+  logbtn:{
+    marginTop:40,
+    paddingTop:7,
+    paddingBottom:6,
+    marginHorizontal:90,
+    backgroundColor:"lightblue",
+    borderRadius:5,
+    width:70,
+    alignItems:'center',
+    justifyContent:'center'
+    
+  },
+  regbtn:{
+    marginTop:40,
+    paddingTop:7,
+    paddingBottom:6,
+    marginHorizontal:90,
+    backgroundColor:"lightblue",
+    borderRadius:5,
+    width:70,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderColor:'black'
+    
+  }
 });
---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAjWTOFLEpg2eHh86pkdV1msKgJI8f6KB0",
+  authDomain: "glogin-5a6fc.firebaseapp.com",
+  projectId: "glogin-5a6fc",
+  storageBucket: "glogin-5a6fc.appspot.com",
+  messagingSenderId: "608048351701",
+  appId: "1:608048351701:web:ae6f0cb103554b7242d55c"
+};
+
+// Initialize Firebase
